d17ccc5cdd2abc137a6adb69264d15c3dd412f8e
Ahmed Fathi <ahmed.fathiie@gmail.com>
2023-08-18 16:33:32 +0300

    Create run_training_siamese_resnet.sh

---
+++ run_training_siamese_resnet.sh
@@ -0,0 +1,7 @@
+# This file was created by IDLab-MEDIA, Ghent University - imec, in collaboration with GRIP-UNINA
+
+#!/bin/bash
+echo "START PYTHON SCRIPT TRAINING"
+
+current_dir=$(pwd)
+python3 ./code/train_network_resnet.py $current_dir $current_dir
